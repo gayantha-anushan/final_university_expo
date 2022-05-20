fcf99b67da223cddf5e2c6b0c2294425d0a8cd78
RuwanBandara96 <bandararuwan419@gmail.com>
2022-05-20 11:00:57 +0530

    changing view profile & Your post screen

--- Screens/ViewProfile.js
+++ Screens/ViewProfile.js
@@ -8,19 +8,19 @@ const ViewProfile = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={styles.titlebar}>
-                    <TouchableOpacity onPress={()=>navigation.navigate("Interface")}>
-                        <AntDesign name="left" size={35} color="black" />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={()=>navigation.navigate("Message")}>
-                        <AntDesign name="message1" size={32} color="black" />
-                    </TouchableOpacity>
-                    
-                </View>
-                <View style={{alignSelf:"center"}}>
-                    <View style={styles.profileimage}>
-                        <Image source={require('../assets/profile.jpg')} style={styles.image} resizeMode="center"></Image>
+                <View style={styles.upcont}>
+                    <View style={styles.titlebar}>
+                        <TouchableOpacity onPress={()=>navigation.navigate("Interface")}>
+                            <AntDesign name="left" size={35} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>navigation.navigate("Message")}>
+                            <AntDesign name="message1" size={32} color="black" />
+                        </TouchableOpacity>
                     </View>
+                    <View style={{alignSelf:"center"}}>
+                        <View style={styles.profileimage}>
+                            <Image source={require('../assets/profile.jpg')} style={styles.image} resizeMode="center"></Image>
+                        </View>
                     <View style={styles.add}>
                         <TouchableOpacity>
                             <AntDesign name="pluscircle" size={50} color="black" style={{ marginTop: 130, marginLeft: 150 }}></AntDesign>
@@ -29,45 +29,49 @@ const ViewProfile = ({ navigation }) => {
                     </View>
                     <Text style={styles.text}>Amal Srinath</Text>
                     
+                    </View>
                 </View>
-                <View style={styles.textset}>
-                    <AntDesign name="user" size={25} color="black"></AntDesign>
-                    <Text style={styles.text1}>Postion As:</Text>
-                    <Text style={styles.text2}>Farmer</Text>
-                </View>
-                <View style={styles.textset}>
-                    <AntDesign name="carryout" size={25} color="black"></AntDesign>
-                    <Text style={styles.text1}>Worked At:</Text>
-                    <Text style={styles.text2}>Amarasena Farmers</Text>
-                </View>
-                <View style={styles.textset}>
-                    <AntDesign name="tags" size={25} color="black"></AntDesign>
-                    <Text style={styles.text1}>Location:</Text>
-                    <Text style={styles.text2}>NuwaraEliya</Text>
-                </View>
-                <View style={styles.textset}>
-                    <AntDesign name="home" size={25} color="black"></AntDesign>
-                    <Text style={styles.text1}>Address:</Text>
-                    <Text style={styles.text2}>294/A Silva Rd,NuwaraEliya</Text>
-                </View>     
-                <View style={styles.textset}>
-                    <AntDesign name="phone" size={25} color="black"></AntDesign>
-                    <Text style={styles.text1}>Phone Number:</Text>
-                    <Text style={styles.text2}>0773456272</Text>
-                </View> 
-                <View style={styles.ButtonCont1}>
-                    <TouchableOpacity style={styles.touchable} onPress={()=>navigation.navigate('ViewPost')}>
-                        <AntDesign name="isv" size={25} color="blue"></AntDesign>
-                    <Text style={styles.text1}>Your Post</Text>
-                    </TouchableOpacity>
-                </View>
+                <View style={styles.downcont}>
+                    <View style={styles.textset}>
+                        <AntDesign name="user" size={25} color="black"></AntDesign>
+                        <Text style={styles.text1}>Postion As:</Text>
+                        <Text style={styles.text2}>Farmer</Text>
+                    </View>
+                    <View style={styles.textset}>
+                        <AntDesign name="carryout" size={25} color="black"></AntDesign>
+                        <Text style={styles.text1}>Worked At:</Text>
+                        <Text style={styles.text2}>Amarasena Farmers</Text>
+                    </View>
+                    <View style={styles.textset}>
+                        <AntDesign name="tags" size={25} color="black"></AntDesign>
+                        <Text style={styles.text1}>Location:</Text>
+                        <Text style={styles.text2}>NuwaraEliya</Text>
+                    </View>
+                    <View style={styles.textset}>
+                        <AntDesign name="home" size={25} color="black"></AntDesign>
+                        <Text style={styles.text1}>Address:</Text>
+                        <Text style={styles.text2}>294/A Silva Rd,NuwaraEliya</Text>
+                    </View>     
+                    <View style={styles.textset}>
+                        <AntDesign name="phone" size={25} color="black"></AntDesign>
+                        <Text style={styles.text1}>Phone Number:</Text>
+                        <Text style={styles.text2}>0773456272</Text>
+                    </View> 
+                    <View style={styles.ButtonCont1}>
+                        <TouchableOpacity style={styles.touchable} onPress={()=>navigation.navigate('ViewPost')}>
+                            <AntDesign name="isv" size={25} color="blue"></AntDesign>
+                        <Text style={styles.text3}>Your Post</Text>
+                        </TouchableOpacity>
+                    </View>
 
-                <View style={styles.ButtonCont}>   
-                    <TouchableOpacity style={styles.touchable}  onPress={() => navigation.navigate("Profile", { state: "EDIT" })}>
-                        <AntDesign name="edit" size={25} color="blue"></AntDesign>
-                        <Text style={styles.text1}>Edit Profile Details</Text>
-                    </TouchableOpacity>
+                    <View style={styles.ButtonCont}>   
+                        <TouchableOpacity style={styles.touchable}  onPress={() => navigation.navigate("Profile", { state: "EDIT" })}>
+                            <AntDesign name="edit" size={25} color="blue"></AntDesign>
+                            <Text style={styles.text3}>Edit Profile Details</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
+                
                 
             </ScrollView>
       </SafeAreaView>
@@ -79,8 +83,11 @@ export default ViewProfile
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#48d1cc',
     },
+    upcont: {
+        backgroundColor:'#48d1cc'
+    },
+
     titlebar: {
         flexDirection: 'row',
         marginTop: 45,
@@ -116,11 +123,16 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginStart: 10,
         fontWeight: 'bold',
-        color:"white"
+        color:'#008b8b'
     },
     text2: {
         fontSize: 20,
         color:"black"
+    },
+    text3: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight:'bold'
     },
     textset: {
         flexDirection: 'row',
@@ -128,22 +140,27 @@ const styles = StyleSheet.create({
         paddingTop:25
     },
     touchable: {
-        backgroundColor: '#e9967a',
+        backgroundColor: '#cd5c5c',
         padding: 10,
         borderRadius: 20,
         paddingHorizontal: 40, 
-        flexDirection:'row'
+        flexDirection: 'row',
+        
+        
     },
     ButtonCont: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-around',
-        paddingTop:30
+        justifyContent: 'center',
+        paddingTop: 30,
+        
+
     },
     ButtonCont1: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        paddingTop:40
+        paddingTop: 55,
+        
     }
 })
