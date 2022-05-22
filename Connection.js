9781187de5504ae0288a8e74de8f9cf416a48764
geoneth tricks <geontricks@gmail.com>
2022-05-22 18:50:12 +0530

    make it better

--- Connection.js
+++ Connection.js
@@ -1,10 +1,6 @@
 function getConnection(){
     return "https://agro-support.herokuapp.com";
-<<<<<<< HEAD
     //return "http://localhost:3000"
-=======
-    //return "http://192.168.8.188:3000"
->>>>>>> 88600e0e504a5dcdde5117dede9748b087b65ba7
 }
 
 module.exports = { getConnection }
