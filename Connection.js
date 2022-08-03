c0e3a82f65ebfaf48927f29f7b403289e1a446f9
RuwanBandara96 <bandararuwan419@gmail.com>
2022-08-03 15:26:50 +0530

    update

--- Connection.js
+++ Connection.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 function getConnection() {
     //return "https://agro-support.herokuapp.com";
     return "http://192.168.43.201:3001"
-=======
-function getConnection(){
-    //return "https://agro-support.herokuapp.com";
-    return "http://192.168.8.188:3001"
->>>>>>> gayantha
 }
 
 module.exports = { getConnection }
