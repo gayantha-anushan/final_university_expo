05b3a6b890b04f77a44ed357cb89299305c8d919
geoneth tricks <geontricks@gmail.com>
2022-09-03 11:53:05 +0530

    Version 1

--- Connection.js
+++ Connection.js
@@ -1,6 +1,6 @@
 function getConnection(){
-    // return "https://agro-support.herokuapp.com";
-    return "http://192.168.8.181:3001"
+    return "https://agro-support.herokuapp.com";
+    //return "http://192.168.8.189:3001"
     // return "http://192.168.8.102:3001"
 }
 
